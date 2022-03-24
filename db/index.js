aa3686c4019084a1ce9034ff800d6ab0a2ac96db
David Wu <dwu0508@gmail.com>
2022-03-24 09:52:08 -0500

    changed database name

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect('mongodb://127.0.0.1:27017/tasksDatabase')
+    .connect('mongodb://127.0.0.1:27017/themeParkDatabase')
     .then(() => {
         console.log('Successfully connected to MongoDb')
     })
